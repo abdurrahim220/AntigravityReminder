e09a5b7fe1168ebad8ed44f1284e7ca7b0bdb187
abdurrahim220 <z.abdurrahim5@gmail.com>
2026-04-09 09:00:45 +0600

    react navigation completly added

---
+++ src/screens/AddReminderScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddReminderScreen = () => {
+  return (
+    <View>
+      <Text>AddReminderScreen</Text>
+    </View>
+  )
+}
+
+export default AddReminderScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigation/AppNavigator'
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+const HomeScreen = ({navigation}:HomeScreenProps) => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button title="Add Reminder" onPress={() => navigation.navigate('AddReminder')} />
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
