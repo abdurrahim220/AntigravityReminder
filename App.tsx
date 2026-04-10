5102f094ec3c5c4e4812543e4549ef7a16579ae7
abdurrahim220 <z.abdurrahim5@gmail.com>
2026-04-10 15:53:59 +0600

    updated the ui

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { store } from './src/app/store';
 import { Provider } from 'react-redux';
 import RootLayout from './src/components/RootLayout';
 import 'react-native-gesture-handler';
-import "./global.css"
 function App() {
   
   return (
